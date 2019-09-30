972982d9b199a9c3c64d44e91117a86caadf7633
David <davidkofiahensackey@gmail.com>
2019-09-30 03:47:28 +0000

    add .

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+
 const API_URL = environment.apiUrl;
 const API_KEY = environment.apiKey;
 
