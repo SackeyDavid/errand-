b51ac854ead6555cc0267a967a4dca2e505ab253
David <davidkofiahensackey@gmail.com>
2019-10-02 04:14:05 +0000

    add .

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
-import { NewsService } from '../news.service';
+import { Component, OnInit } from '@angular/core';
+import * as list from '../../assets/js/data.js';
+// import { NewsService } from '../news.service';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor(private newsService: NewsService) {}
+  constructor() {}
 
+  // private newsService: NewsService)
   ngOnInit() {
-    console.log(this.newsService.currentArticle);
+    //console.log(this.newsService.currentArticle);
+    list();
   }
 
 }
