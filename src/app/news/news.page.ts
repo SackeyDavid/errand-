f47e299396021d2bd577a705d9369a5ea48bc04a
David <davidkofiahensackey@gmail.com>
2019-10-09 18:06:18 +0000

    commit

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-news',
@@ -10,32 +11,32 @@ export class NewsPage implements OnInit {
   messages = [
     {
       user: 'david',
-      createdAt: '1554090856000',
+      createdAt: 1554090856000,
       msg: 'Hey whats up?'
     },
     {
       user: 'Finn',
-      createdAt: '1554090956000',
+      createdAt: 1554090956000,
       msg: 'cool bro and u?'
     },
     {
       user: 'david',
-      createdAt: '1554090856000',
+      createdAt: 1554090856000,
       msg: 'Good, you said 2 packs right?'
     },
     {
       user: 'Finn',
-      createdAt: '1554091056000',
+      createdAt: 1554091056000,
       msg: 'Yeah, at src now. Will be waiting'
     },
     {
       user: 'david',
-      createdAt: '1554091256000',
+      createdAt: 1554091256000,
       msg: 'okay, will be there with it soon'
     },
     {
       user: 'Finn',
-      createdAt: '1554091356000',
+      createdAt: 1554091356000,
       msg: '...waiting'
     }
   ]
@@ -43,13 +44,26 @@ export class NewsPage implements OnInit {
   currentUser = 'david';
 
   newMsg = ''; 
-
-  sendMessage() {
-
-  }
+  @ViewChild(IonContent, {static: false}) ionContent: IonContent;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sendMessage() {
+    this.messages.push({
+      user:'david',
+      createdAt: new Date().getTime(),
+      msg: this.newMsg
+    });
+
+    this.newMsg = '';
+
+    setTimeout(()=> {
+      this.ionContent.scrollToBottom(200);
+    });
+    
   }
 
 }
